f51d61f92e24186d2cce0b29a96ac53fda50c01a
kent <164713332@qq.com>
2025-04-25 07:20:55 +0000

    Fix bug.

--- src/ClaimManager.php
+++ src/ClaimManager.php
@@ -68,7 +68,7 @@ class ClaimManager implements ClaimManagerInterface {
           $account,
           LedgerInterface::AMOUNT_TYPE_DEBIT,
           $reward->getAmount(),
-          "Task {$reward->get('task_id')->entity->label()} finished, get reward {$reward->label()}",
+          "Claim reward {$reward->label()}",
           $claim
         );
 
